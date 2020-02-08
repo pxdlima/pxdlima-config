0b53545c22afcb09de2e15e45699d74f2c8ee18e
Peter V <21083961+pxdlima@users.noreply.github.com>
2020-02-08 11:45:23 +0800

    Update sevpn.sh

--- sevpn.sh
+++ sevpn.sh
@@ -4,7 +4,7 @@ echo ""
 echo "Initiating Setup..."
 
 apt update
-apt install build-essential wget -y
+apt install build-essential wget curl -y
 apt upgrade -y
 wget http://www.softether-download.com/files/softether/v4.32-9731-beta-2020.01.01-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.32-9731-beta-2020.01.01-linux-x64-64bit.tar.gz
 tar xvzf softether-vpnserver* && rm -rf softether-vpnserver*
