064b86140c38d59095a5b9af60351f909e80beeb
Peter V <21083961+pxdlima@users.noreply.github.com>
2020-02-10 02:12:11 +0800

    Update sdbx.sh
    Tested to work ONLY with Debian 9 and 10.

--- sdbx.sh
+++ sdbx.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
-mkdir /usr/local/utorrent && cd /usr/local/utorrentnetsta
-wget http://download-hr.utorrent.com/track/beta/endpoint/utserver/os/linux-x64-debian-7-0
+rm -rf /usr/local/utorrent && rm /usr/bin/utserver
+echo "Removing previous uTorrent Server installation..."
+sleep 3
+clear
+cd /usr/local/
+echo "Downloading zipped binary.."
+wget -q http://download-hr.utorrent.com/track/beta/endpoint/utserver/os/linux-x64-debian-7-0
+sleep 2
+tar xvf linux-x64-debian-7-0
 rm linux-x64-debian-7-0
-tar zxvf linux-x64-debian-7-0
+mv /usr/local/utorrent-server-alpha-v3_3/ /usr/local/utorrent && cd /usr/local/utorrent
 apt-get install libssl-dev
-wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
+wget -q http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
 dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
 rm libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
+echo "Setting up server settings.."
+touch dht.dat
+touch rss.dat
+touch resume.dat
+touch webcache.dat
+sleep 2
+wget -q https://github.com/pxdlima/pxdlima-config/raw/master/settings.dat
+ln -s /usr/local/utorrent/utserver /usr/bin/utserver
+echo "Completing installation and starting up server.."
+sleep 2
+utserver -logfile /usr/local/utorrent/server.log -daemon
