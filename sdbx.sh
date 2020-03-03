460fd8d96f227a3c915a51d850517309cd512089
Peter V <21083961+pxdlima@users.noreply.github.com>
2020-03-03 20:41:42 +0800

    Update sdbx.sh

--- sdbx.sh
+++ sdbx.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+clear
+if [ -d /usr/local/utorrent ]; then
+	echo "uTorrent detected.. Exit"
+else
 rm -rf /usr/local/utorrent && rm /usr/bin/utserver
 ln -fs /usr/share/zoneinfo/Asia/Manila /etc/localtime
 echo "Removing previous uTorrent Server installation..."
-sleep 3
-clear
+sleep 2
 cd /usr/local/
 echo "Downloading zipped binary.."
 wget -q http://download-hr.utorrent.com/track/beta/endpoint/utserver/os/linux-x64-debian-7-0
@@ -15,7 +18,7 @@ apt install libssl-dev -y
 wget -q http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
 dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
 rm libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
-echo "Setting up server settings.."
+echo "Adding settings.."
 touch dht.dat
 touch rss.dat
 touch resume.dat
@@ -23,6 +26,7 @@ touch webcache.dat
 sleep 2
 wget -q https://github.com/pxdlima/pxdlima-config/raw/master/settings.dat
 ln -s /usr/local/utorrent/utserver /usr/bin/utserver
-echo "Completing installation and starting up server.."
+echo "Starting services.."
 sleep 2
+fi
 utserver -logfile /usr/local/utorrent/server.log -daemon
