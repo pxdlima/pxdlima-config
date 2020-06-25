6315aaa8458e65e734ed35977e9a8da5be243542
Peter V <21083961+pxdlima@users.noreply.github.com>
2020-06-26 07:51:37 +0800

    Update sevpn.sh

--- sevpn.sh
+++ sevpn.sh
@@ -9,7 +9,7 @@ echo "Initiating Setup..."
 apt update
 apt install build-essential wget curl -y
 apt upgrade -y
-wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.32-9731-beta/softether-vpnserver-v4.32-9731-beta-2020.01.01-linux-x64-64bit.tar.gz
+wget https://www.softether-download.com/files/softether/v4.34-9745-rtm-2020.04.05-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.34-9745-rtm-2020.04.05-linux-x64-64bit.tar.gz
 tar xvzf softether-vpnserver* && rm -rf softether-vpnserver*
 cd vpnserver && make i_read_and_agree_the_license_agreement && rm *.txt && curl -Os https://raw.githubusercontent.com/pxdlima/trifle/master/vpn_server.config
 cd .. && mv vpnserver /usr/local && chmod 700 /usr/local/vpnserver/vpncmd && chmod 700 /usr/local/vpnserver/vpnserver
