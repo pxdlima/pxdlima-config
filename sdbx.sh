efec5c7bb46a3643cb69e5df5a7790d934e84698
P Paul <21083961+pxdlima@users.noreply.github.com>
2020-10-09 11:59:18 +0800

    Update sdbx.sh

--- sdbx.sh
+++ sdbx.sh
@@ -11,12 +11,12 @@ ln -fs /usr/share/zoneinfo/Asia/Manila /etc/localtime
 cd /usr/local/
 echo "Downloading zipped binary.."
 wget -q http://download-hr.utorrent.com/track/beta/endpoint/utserver/os/linux-x64-debian-7-0
-wget -q http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
+wget -q http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
 DEBIAN_FRONTEND=noninteractive apt-get -yqq install libssl-dev 
 sleep 1
 tar xf linux-x64-debian-7-0 
-dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb > /dev/null
-rm linux-x64-debian-7-0 && rm libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb
+dpkg -i libssl1.0.0* > /dev/null
+rm linux-x64-debian-7-0 && rm *.deb
 mv /usr/local/utorrent-server-alpha-v3_3/ /usr/local/utorrent && cd /usr/local/utorrent
 echo "Configuring startup files."
 touch dht.dat
