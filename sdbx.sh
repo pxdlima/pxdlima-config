472940d0d34b99e85b79777fce2c40ad6156cee7
Pedro V <21083961+ppdlv@users.noreply.github.com>
2020-12-21 05:48:16 +0800

    Update sdbx.sh

--- sdbx.sh
+++ sdbx.sh
@@ -11,7 +11,7 @@ ln -fs /usr/share/zoneinfo/Asia/Manila /etc/localtime
 cd /usr/local/
 echo "Downloading zipped binary.."
 wget -q http://download-hr.utorrent.com/track/beta/endpoint/utserver/os/linux-x64-debian-7-0
-wget -q http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
+wget -q http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5_amd64.deb
 DEBIAN_FRONTEND=noninteractive apt-get -yqq install libssl-dev 
 sleep 1
 tar xf linux-x64-debian-7-0 
@@ -25,6 +25,17 @@ touch resume.dat
 touch webcache.dat
 wget -q https://github.com/pxdlima/trifle/raw/master/settings.dat
 ln -s /usr/local/utorrent/utserver /usr/bin/utserver > /dev/null
-utserver -logfile /usr/local/utorrent/server.log -daemon
+echo "[Unit]
+Description=uTorrent service.
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/utserver -settingspath /usr/local/utorrent/ -logfile /usr/local/utorrent/server.log
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/utorrent.service
+systemctl daemon-reload
+systemctl enable utorrent.service
+service utorrent start 
 echo "Installation Complete!"
 fi
