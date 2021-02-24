16fc318ef5ba216a629f6a0c97af99b13d1f6ae2
Pedro V <21083961+ppdlv@users.noreply.github.com>
2021-02-24 12:29:44 +0800

    Update sdbx.sh

--- sdbx.sh
+++ sdbx.sh
@@ -12,7 +12,7 @@ cd /usr/local/
 echo "Downloading zipped binary.."
 wget -q http://download-hr.utorrent.com/track/beta/endpoint/utserver/os/linux-x64-debian-7-0
 wget -q http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5_amd64.deb
-DEBIAN_FRONTEND=noninteractive apt-get -yqq install libssl-dev 
+DEBIAN_FRONTEND=noninteractive apt-get -yqq install libssl-dev
 sleep 1
 tar xf linux-x64-debian-7-0 
 dpkg -i libssl1.0.0* > /dev/null
@@ -30,12 +30,21 @@ Description=uTorrent service.
 
 [Service]
 Type=simple
+ExecStartPre=/bin/sleep 10
 ExecStart=/usr/bin/utserver -settingspath /usr/local/utorrent/ -logfile /usr/local/utorrent/server.log
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/utorrent.service
 systemctl daemon-reload
 systemctl enable utorrent.service
-service utorrent start 
+systemctl start utorrent
+apt update
+apt -y upgrade
+apt install nginx -y
+apt autoremove
+systemctl stop nginx.service
+rm -rf /etc/nginx/sites-enabled/*
+curl -so /etc/nginx/conf.d/utorrent.conf https://raw.githubusercontent.com/ppdlv/trifle/master/files/webui.conf
+systemctl restart nginx.service
 echo "Installation Complete!"
 fi
